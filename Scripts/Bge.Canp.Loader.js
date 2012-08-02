e005bdea48b63bd4eb806b52c247d05bcc41ea01
vamsim <vamc009@gmail.com>
2012-08-02 05:33:25 -0400

    non cookie

--- Scripts/Bge.Canp.Loader.js
+++ Scripts/Bge.Canp.Loader.js
@@ -4,6 +4,8 @@ var CanpCommEMsg = 'Data not available at this time. Please try again later';   
 var CanpGenEMsg = 'Error detected. Please contact your BGE service representative.'; // generic server error
 var globalEMsg = ''; //to store the global error message
 
+var BGELoggedIn = false;
+
 jQuery.support.cors = true;
 
 function ADLogout() {
@@ -13,14 +15,14 @@ function ADLogout() {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         timeout: CanpAjaxTO,
-		crossDomain: true,
-		async:false,
+        crossDomain: true,
+        async: false,
         global: false
     });
 }
 
 function ADLogin(data) {
-	$.cookie("ASP.NET_SessionId", null);
+    $.cookie("ASP.NET_SessionId", null);
     $.ajax({
         type: "POST",
         url: "http://stage-newsletter.bge.com/RenderService.asmx/ADLogin",
@@ -28,9 +30,9 @@ function ADLogin(data) {
         dataType: "json",
         data: data,
         timeout: CanpAjaxTO,
-		crossDomain: true,
+        crossDomain: true,
         global: false,
-		async:false,
+        async: false,
         success: LoginSuccess
     });
 }
@@ -42,6 +44,7 @@ function LoginSuccess(msg) {
         return;
     } else if (msg.d.status) {
         $.cookie("loggedin", "true");
+        BGELoggedIn = true;
         return;
     }
 
@@ -55,9 +58,9 @@ function GetAccountSummary() {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         timeout: CanpAjaxTO,
-		async:false,
+        async: false,
         global: false,
-		crossDomain: true,
+        crossDomain: true,
         success: AcctSumPopulate,
         error: AcctSumError,
         complete: AcctSumComplete
@@ -65,15 +68,17 @@ function GetAccountSummary() {
 }
 function AcctSumPopulate(msg) {
     if (jQuery.isEmptyObject(msg.d)) {
-		$("#AccountSummaryPage #uiSessionStatusAcctSum").text("Error");
+        $("#AccountSummaryPage #uiSessionStatusAcctSum").text("Error");
         $("#AccountSummaryPage #uiSessionStatusAcctSum").show();
-		$.cookie("loggedin", null);
+        $.cookie("loggedin", null);
+        BGELoggedIn = false;
         return;
 
     } else if (!jQuery.isEmptyObject(msg.d.ErrorMessage)) {
         $("#AccountSummaryPage #uiSessionStatusAcctSum").show();
         $("#AccountSummaryPage #uiSessionStatusAcctSum").text(msg.d.ErrorMessage);
-		$.cookie("loggedin", null);
+        $.cookie("loggedin", null);
+        BGELoggedIn = false;
         return;
     }
     $('#AccountSummaryPage #uiAccountId').text("Acct# " + msg.d.AccountId);
@@ -118,108 +123,108 @@ function AcctSumPopulate(msg) {
         $('#AccountSummaryPage #uiPremiseSingle').text('no premise');
         $('#AccountSummaryPage #uiNoPremise').show();
     }
-	$("#AccountSummaryPage #uiSessionStatusAcctSum").hide();
+    $("#AccountSummaryPage #uiSessionStatusAcctSum").hide();
 }
 function AcctSumError(xhr, ajaxOptions, thrownError) {
-   
+
 }
 function AcctSumComplete(xhr, status) {
     if (status == 'timeout') {
         $("#AccountSummaryPage #uiSessionStatusAcctSum").show();
-		$("#AccountSummaryPage #uiSessionStatusAcctSum").text("Timeout");
+        $("#AccountSummaryPage #uiSessionStatusAcctSum").text("Timeout");
     }
     else if (status != 'success') {
         $("#AccountSummaryPage #uiSessionStatusAcctSum").show();
-		$("#AccountSummaryPage #uiSessionStatusAcctSum").text("Error");
+        $("#AccountSummaryPage #uiSessionStatusAcctSum").text("Error");
     }
 }
 
-$( '#homePage' ).live( 'pageshow', function () {
+$('#homePage').live('pageshow', function () {
 
- $("#homePage #uiSignButton").click(function () {
-	 var userId = $('#homePage input[name=userId]');
-	 var password = $('#homePage input[name=password]');
-	 //var data = 'userId=' + userId.val() + '&password=' + password.val();
-	 var jsonData = { "userId": userId.val(), "password": password.val() };
-	 var data = JSON.stringify(jsonData)
-	 ADLogin(data);
-	 if ($.cookie("loggedin") === 'true') {
-		 $("#uiAccountLoggedOut").hide();
-		 $("#uiAccountLoggedIn").show();
-		 $("#uiLogoutButton").show();
-		 $("#uiLoginButton").hide();
-		 $.mobile.changePage("AccountSummary.htm", { transition: "slide"});
-	 }
-	 else {
-		 $("#homePage #uiLblError").show();
-		 $("#homePage #uiLblError").text("Invalid UserId/Password");
-	 }
-	 return false;
- });
+    $("#homePage #uiSignButton").click(function () {
+        var userId = $('#homePage input[name=userId]');
+        var password = $('#homePage input[name=password]');
+        //var data = 'userId=' + userId.val() + '&password=' + password.val();
+        var jsonData = { "userId": userId.val(), "password": password.val() };
+        var data = JSON.stringify(jsonData)
+        ADLogin(data);
+        if (BGELoggedIn) {
+            $("#uiAccountLoggedOut").hide();
+            $("#uiAccountLoggedIn").show();
+            $("#uiLogoutButton").show();
+            $("#uiLoginButton").hide();
+            $.mobile.changePage("AccountSummary.htm", { transition: "slide" });
+        }
+        else {
+            $("#homePage #uiLblError").show();
+            $("#homePage #uiLblError").text("Invalid UserId/Password");
+        }
+        return false;
+    });
 
- if ($.cookie("loggedin") === 'true') {
-	 $("#homePage #uiAccountLoggedOut").hide();
-	 $("#homePage #uiAccountLoggedIn").show();
-	 $("#homePage #uiLogoutButton").show();
-	 $("#homePage #uiLoginButton").hide();
- }else{
-	$("#homePage #uiAccountLoggedOut").show();
-	 $("#homePage #uiAccountLoggedIn").hide();
-	 $("#homePage #uiLogoutButton").hide();
-	 $("#homePage #uiLoginButton").show();
- }
- 
+    if (BGELoggedIn) {
+        $("#homePage #uiAccountLoggedOut").hide();
+        $("#homePage #uiAccountLoggedIn").show();
+        $("#homePage #uiLogoutButton").show();
+        $("#homePage #uiLoginButton").hide();
+    } else {
+        $("#homePage #uiAccountLoggedOut").show();
+        $("#homePage #uiAccountLoggedIn").hide();
+        $("#homePage #uiLogoutButton").hide();
+        $("#homePage #uiLoginButton").show();
+    }
 
- $("#homePage #uiLogoutButton").click(function () {
-	 ADLogout();
-	 $("#uiLoginButton").show();
-	 $("#uiLogoutButton").hide();
-	 $.cookie("loggedin", null);
-	 $("#uiAccountLoggedOut").show();
-	 $("#uiAccountLoggedIn").hide();
-	 return false;
- });
+
+    $("#homePage #uiLogoutButton").click(function () {
+        ADLogout();
+        $("#uiLoginButton").show();
+        $("#uiLogoutButton").hide();
+        $.cookie("loggedin", null);
+        BGELoggedIn = false;
+        $("#uiAccountLoggedOut").show();
+        $("#uiAccountLoggedIn").hide();
+        return false;
+    });
 });
 
-$( '#AccountSummaryPage' ).live( 'pageshow', function () {
-	if ($.cookie("loggedin") === 'true') {
-			$('#AccountSummaryPage #uiSessionStatusAcctSum').text("Loading..");
-			$('#AccountSummaryPage #uiSessionStatusAcctSum').show();
-			GetAccountSummary();
-			if ($.cookie("loggedin") !== 'true') {
-				$.mobile.changePage("login.htm", { transition: "slide", reloadPage: true});
-			}
-			
-		}
-	else{
-		$.mobile.changePage("login.htm", { transition: "slide"});
-	}
-	$("#AccountSummaryPage #uiSignoutButton").click(function () {
-		 ADLogout();
-		 $.cookie("loggedin", null);
-		 $.mobile.changePage("index.html", { transition: "slide", reloadPage: true});
-		 return false;
-	 });
-});
+$('#AccountSummaryPage').live('pageshow', function () {
+    if (BGELoggedIn) {
+        $('#AccountSummaryPage #uiSessionStatusAcctSum').text("Loading..");
+        $('#AccountSummaryPage #uiSessionStatusAcctSum').show();
+        GetAccountSummary();
+        if (!BGELoggedIn) {
+            $.mobile.changePage("login.htm", { transition: "slide", reloadPage: true });
+        }
 
-
-$( '#loginPage' ).live( 'pageshow', function () {
-	$("#loginPage #uiSignButtonD").click(function () {
-		var userId = $('#loginPage input[name=userId]');
-		var password = $('#loginPage input[name=password]');
-		//var data = 'userId=' + userId.val() + '&password=' + password.val();
-		var jsonData = { "userId": userId.val(), "password": password.val() };
-		var data = JSON.stringify(jsonData);
-		ADLogin(data);
-		if ($.cookie("loggedin") === 'true') {
-			$.mobile.changePage("AccountSummary.htm", { transition: "slide"});
-		}
-		else {
-			$("#loginPage #uiLblError").show();
-			$("#loginPage #uiLblError").text("Invalid UserId/Password");
-		}
-		return false;
-	});
+    }
+    else {
+        $.mobile.changePage("login.htm", { transition: "slide" });
+    }
+    $("#AccountSummaryPage #uiSignoutButton").click(function () {
+        ADLogout();
+        $.cookie("loggedin", null);
+        BGELoggedIn = false;
+        $.mobile.changePage("index.html", { transition: "slide", reloadPage: true });
+        return false;
+    });
 });
 
 
+$('#loginPage').live('pageshow', function () {
+    $("#loginPage #uiSignButtonD").click(function () {
+        var userId = $('#loginPage input[name=userId]');
+        var password = $('#loginPage input[name=password]');
+        //var data = 'userId=' + userId.val() + '&password=' + password.val();
+        var jsonData = { "userId": userId.val(), "password": password.val() };
+        var data = JSON.stringify(jsonData);
+        ADLogin(data);
+        if (BGELoggedIn) {
+            $.mobile.changePage("AccountSummary.htm", { transition: "slide" });
+        }
+        else {
+            $("#loginPage #uiLblError").show();
+            $("#loginPage #uiLblError").text("Invalid UserId/Password");
+        }
+        return false;
+    });
+});
